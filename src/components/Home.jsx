0872e8adf80da33779a0b6a0686b8d32ae41f620
diego_lima <lima.j.diego@gmail.com>
2021-08-06 09:21:17 -0300

    Criado component home e importando no App.js

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react'
+
+export default ()=>{
+
+    return(
+        <Fragment>
+            <h3>Clima no seu local</h3>
+            <hr />
+            <ul>
+                <li>Temperatura atual: xº</li>
+                <li>Temperatura Máxima</li>
+                <li>Temperatura Minima</li>
+                <li>Prexão: x hpa</li>
+                <li>Umidade: x%</li>
+            </ul>
+
+        </Fragment>
+    )
+}
