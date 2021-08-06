ce715ac06db83b5776a5280c43a060ab9e762d41
diego_lima <lima.j.diego@gmail.com>
2021-08-06 09:48:28 -0300

    implementado condicional caso a localizaçao esteja desativada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,24 +4,29 @@ export default ()=>{
     const [location, setLocation] = useState(false)
 
     useEffect(()=>{
-        navigator.geolocation.getCurrentPosition((position)=>{
+        navigator.geolocation.getCurrentPosition((position)=>{ // pede permissão para o navegador e pegar as coordenadas
             console.log(position.coords.latitude, position.coords.longitude);
             setLocation(true)
         })
     }, [])
 
-    return(
-        <Fragment>
-            <h3>Clima no seu local</h3>
-            <hr />
-            <ul>
-                <li>Temperatura atual: xº</li>
-                <li>Temperatura Máxima</li>
-                <li>Temperatura Minima</li>
-                <li>Prexão: x hpa</li>
-                <li>Umidade: x%</li>
-            </ul>
-
-        </Fragment>
-    )
+    if(location == false){
+        return(
+            <h1>Você precisa habilitar a localização em seu navegador</h1>
+        )
+    }else{
+        return(
+            <Fragment>
+                <h3>Clima no seu local</h3>
+                <hr />
+                <ul>
+                    <li>Temperatura atual: xº</li>
+                    <li>Temperatura Máxima: xº</li>
+                    <li>Temperatura Minima: xº</li>
+                    <li>Pressão: x hpa</li>
+                    <li>Umidade: x%</li>
+                </ul>    
+            </Fragment>
+        );
+    }    
 }
